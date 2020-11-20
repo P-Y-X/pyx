d94ba1c81451c468509bb72bb4063613f37e2aab
Grigory Malivenko <nerox8664@gmail.com>
2020-11-20 20:30:53 +0200

    Add auth / push / pull methods.

--- pyx/cli.py
+++ pyx/cli.py
@@ -1,6 +1,66 @@
 import argparse
 
 
+__PYX_CONFIG__ = {
+    'api_url': 'http://127.0.0.1:8888/'
+}
+
+
+def _load_config():
+    """
+    Load JSON config from ~/.pyx/pyx.json
+    """
+    import os
+    import json
+
+    config_path_dir = os.path.join(os.path.expanduser('~'), '.pyx')
+    config_path = os.path.join(config_path_dir, 'pyx.json')
+
+    if os.path.exists(config_path):
+        global __PYX_CONFIG__
+        __PYX_CONFIG__ = {**__PYX_CONFIG__, **json.load(open(config_path, 'r'))}
+
+
+def _save_config():
+    """
+    Save JSON config to ~/.pyx/pyx.json
+    """
+    import os
+    import json
+
+    config_path_dir = os.path.join(os.path.expanduser('~'), '.pyx')
+    config_path = os.path.join(config_path_dir, 'pyx.json')
+
+    os.makedirs(config_path_dir, exist_ok=True)
+    with open(config_path, 'w') as f:
+        f.write(json.dumps(__PYX_CONFIG__, indent=4))
+        f.close()
+
+
+def auth(args):
+    """
+    Authorization
+    """
+    __PYX_CONFIG__['user_token'] = args.user_token
+
+    # TODO: check user creds
+    import requests
+    from urllib.parse import urljoin
+    headers = {'user_token': __PYX_CONFIG__["user_token"]}
+
+    r = requests.get(
+        urljoin(__PYX_CONFIG__["api_url"], 'auth'),
+        params={},
+        headers=headers
+    )
+
+    if r.status_code == 200:
+        print('Authorized.')
+        _save_config()
+    else:
+        print('Wrong token.')
+
+
 def create(args):
     """
     Create new project
@@ -36,72 +96,134 @@ def test(*args, **kwargs):
 
     if not os.path.exists('PYX.py'):
         print('Cant find PYX.py. Are you in a pyx-project directory?')
+        return False
     else:
-        print('Testing project ...')
-        import sys
-        sys.path.append('./')
-        from PYX import PYXImplementedModel
+        try:
+            print('Testing project ...')
+            import sys
+            sys.path.append('./')
+            from PYX import PYXImplementedModel
 
-        print('Initializing model ...')
-        project = PYXImplementedModel()
-        project.initialize()
-        project.set_weights()
+            print('Initializing model ...')
+            project = PYXImplementedModel()
+            project.initialize()
+            project.set_weights()
 
-        print('inputs: ', project.get_input_shapes())
-        print('inputs: ', project.get_input_types())
-        print('....')
+            print('inputs: ', project.get_input_shapes())
+            print('inputs: ', project.get_input_types())
+            print('....')
 
-        for i in range(10):
-            test_sample = {}
-            inputs = project.get_input_shapes()
+            for i in range(10):
+                test_sample = {}
+                inputs = project.get_input_shapes()
 
-            for k in inputs:
-                test_sample[k] = np.zeros(inputs[k])
+                for k in inputs:
+                    test_sample[k] = np.zeros(inputs[k])
 
-            start = time.time()
-            _ = project.predict(test_sample)
-            end = time.time()
-            print('Inference time: ', end - start)
+                start = time.time()
+                _ = project.predict(test_sample)
+                end = time.time()
+                print('Inference time: ', end - start)
 
-        print('....')
-        print('PASSED')
+            print('....')
+            print('PASSED')
+        except:
+            print('....')
+            print('An error occured.')
+            return False
+
+        return True
 
 
 def push(args, **kwargs):
+    if not test():
+        print('Can not submit without testing first.')
+
     import requests
+    from urllib.parse import urljoin
     import shutil
     import os
-    shutil.make_archive('_project', 'zip', '.')
+    import tempfile
 
-    fileobj = open('_project.zip', 'rb')
-    r = requests.post(args.magic_url, data={}, files={"archive": ("project.zip", fileobj)})
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print('Packing current project ...')
+        shutil.make_archive(os.path.join(tmpdirname, '_project'), 'zip', '.')
 
-    os.unlink('_project.zip')
+        print('Uploading data ...')
+        model_id, framework = args.model_name.split('/')
+        fileobj = open(os.path.join(tmpdirname, '_project.zip'), 'rb')
+        headers = {'user_token':  __PYX_CONFIG__["user_token"]}
+        r = requests.post(urljoin(__PYX_CONFIG__["api_url"], 'model/' + model_id + '/upload/' + framework),
+                          headers=headers,
+                          files={"project_files": ("project.zip", fileobj)})
+
+        if r.status_code == 200:
+            print('Succesfully pushed.')
+        else:
+            print('An error occured.')
+
+
+def pull(args, **kwargs):
+    import requests
+    from urllib.parse import urljoin
+    import shutil
+    import os
+    import tempfile
+
+    print('Downloading data ...')
+    model_id, framework = args.model_name.split('/')
+
+    headers = {'user_token':  __PYX_CONFIG__["user_token"]}
+    r = requests.get(urljoin(__PYX_CONFIG__["api_url"], 'model/' + model_id + '/download/' + framework),
+                     headers=headers, stream=True)
+
+    if r.status_code == 200:
+        print('Succesfully pulled ...')
+    else:
+        print('An error occured.')
+        return
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print('Packing current project ...')
+        with open(os.path.join(tmpdirname, 'src.zip'), 'wb') as out_file:
+            shutil.copyfileobj(r.raw, out_file)
+
+        shutil.unpack_archive(os.path.join(tmpdirname, 'src.zip'), model_id)
+
+        print('....')
+        print('DONE')
 
 
 def main():
+    _load_config()
+    print(__PYX_CONFIG__)
     parser = argparse.ArgumentParser(prog='pyx')
 
     subparsers = parser.add_subparsers(dest='mode', help='sub-command help')
+
+    parser_auth = subparsers.add_parser('auth', help='Auth PYX user')
+    parser_auth.add_argument('user_token', type=str, help='set user token')
 
     parser_create = subparsers.add_parser('create', help='Create new PYX project')
     parser_create.add_argument('project_name', type=str, help='a name of new project (e.g. resnet-test)')
     parser_create.add_argument('category', type=str, help='a category of a new project (e.g. cv/classification)')
 
     parser_pull = subparsers.add_parser('pull', help='Pull published workspace')
-    parser_pull.add_argument('magic_url', type=str, help='a magic url from pyx.ai')
+    parser_pull.add_argument('model_name', type=str, help='a magic url from pyx.ai')
 
     parser_push = subparsers.add_parser('push', help='Push current workspace to pyx.ai')
-    parser_push.add_argument('magic_url', type=str, help='a magic url from pyx.ai')
+    parser_push.add_argument('model_name', type=str, help='a magic url from pyx.ai (model-id/framework)')
 
     _ = subparsers.add_parser('test', help='Run tests locally')
 
     params = parser.parse_args()
 
     subprogs = {
+        'auth': auth,
         'create': create,
         'test': test,
         'push': push,
+        'pull': pull,
     }
 
     subprogs[params.mode](params)
