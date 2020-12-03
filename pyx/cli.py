4ce7b15cc4d39668d6b48732ce27f9af7dec4bd0
Grigory Malivenko <nerox8664@gmail.com>
2020-12-04 01:04:33 +0200

    Update CLI.

--- pyx/cli.py
+++ pyx/cli.py
@@ -1,20 +1,27 @@
 import argparse
 import os
 import json
+import inquirer
 
 
 __PYX_CONFIG__ = {
-    'api_url': 'https://beta.pyx.ai/api/'
+    'api_url': 'https://beta.pyx.ai/api/',
+    'frameworks': ['pytorch', 'onnx']
     # 'api_url': 'http://127.0.0.1:8888/api/'
 }
 
 
 __PYX_PROJECT_TEMPLATE__ = {
+    'author': '',
     'project_name': '',
     'model_id': '',
     'category_id': '',
-    'category': '',
-    'author': '',
+    'name': '',
+    'price': '',
+    'paper': '',
+    'description_short': '',
+    'description_full': '',
+    'dataset': '',
     'license': '',
 }
 
@@ -71,6 +78,31 @@ def ensure_have_permissions(func):
     return wrapper_fn
 
 
+def _sync_meta():
+    import requests
+    from urllib.parse import urljoin
+
+    r = requests.get(urljoin(__PYX_CONFIG__["api_url"], 'categories'))
+    categories_json = r.json()['categories']
+
+    __PYX_CONFIG__['categories'] = categories_json
+    _save_config()
+
+
+def _get_template_path(subcategory_id):
+    subcategory_obj = next(obj for obj in __PYX_CONFIG__['categories'] if obj['id'] == subcategory_id)
+    category_obj = next(obj for obj in __PYX_CONFIG__['categories'] if obj['id'] == subcategory_obj['parent_id'])
+
+    return '{}/{}'.format(category_obj['url'], subcategory_obj['url'])
+
+
+def _get_category_choices(answers):
+    root_id = None
+    if 'category' in answers:
+        root_id = answers['category']
+    return [(i['name'], i['id']) for i in __PYX_CONFIG__['categories'] if i['parent_id'] == root_id]
+
+
 def _load_config():
     """
     Load JSON config from ~/.pyx/pyx.json
@@ -99,6 +131,44 @@ def _save_config():
     with open(config_path, 'w') as f:
         f.write(json.dumps(__PYX_CONFIG__, indent=4))
         f.close()
+
+
+def _add_framework(project_path_prefix, subcategory_id, framework, **kwargs):
+    """
+    Add model to the project
+    """
+    import os
+    import pyx
+
+    subcategory_path = _get_template_path(subcategory_id)
+
+    pyx_boilerplate_path = os.path.join(
+        os.path.dirname(pyx.__file__), 'boilerplates',
+        subcategory_path,
+        framework
+    )
+
+    if not os.path.exists(pyx_boilerplate_path):
+        pyx_boilerplate_path = os.path.join(
+            os.path.dirname(pyx.__file__), 'boilerplates', 'general',
+            framework
+        )
+
+    if os.path.exists(pyx_boilerplate_path):
+        print('Adding model ...')
+        try:
+            from shutil import copytree
+            copytree(pyx_boilerplate_path, os.path.join(project_path_prefix, 'models', framework))
+            print('...')
+            print('Your boilerplate is ready to use. Please, run:')
+            print('$ pyx run ' + framework)
+            print('or :')
+            print('$ cd models/{}'.format(framework))
+            print('$ python PYX.py')
+        except FileExistsError:
+            print('Destination directory is already exist. Consider another name of the project.')
+    else:
+        print('Template for category {0} not found'.format(subcategory_path))
 
 
 def auth(args, **kwargs):
@@ -136,24 +206,51 @@ def create(args, **kwargs):
     """
     Create new project
     """
+    _sync_meta()
+    questions = [
+        inquirer.List('category',
+                      message="What is the category of your project?",
+                      choices=_get_category_choices,
+                      ),
+        inquirer.List('subcategory',
+                      message="What is the subcategory of your project?",
+                      choices=_get_category_choices,
+                      ),
+        inquirer.Text('project_name',
+                      message="Please, specify project name"
+                      ),
+        inquirer.Checkbox('frameworks',
+                          message="Prepare boilerplates for specific frameworks",
+                          choices=__PYX_CONFIG__['frameworks']
+                          ),
+    ]
+    answers = inquirer.prompt(questions)
+
+    project_name = answers['project_name']
+    category_id = answers['subcategory']
+    frameworks = answers['frameworks']
+
     import os
     import json
     try:
         print('Creating project ...')
-        os.makedirs(args.project_name)
-        os.makedirs(os.path.join(args.project_name, 'web'))
-        os.makedirs(os.path.join(args.project_name, 'models'))
-        with open(os.path.join(args.project_name, 'pyx.json'), 'w') as f:
-            pyx_project = {**__PYX_PROJECT_TEMPLATE__, 'project_name': args.project_name, 'category': args.category}
+        os.makedirs(project_name)
+        os.makedirs(os.path.join(project_name, 'web'))
+        os.makedirs(os.path.join(project_name, 'models'))
+        with open(os.path.join(project_name, 'pyx.json'), 'w') as f:
+            pyx_project = {**__PYX_PROJECT_TEMPLATE__, 'project_name': project_name, 'category_id': category_id}
             f.write(json.dumps(pyx_project, indent=4))
             f.close()
 
             print('...')
             print('Your project is ready to use. Please go to the project folder:')
-            print('$ cd ' + args.project_name)
+            print('$ cd ' + project_name)
 
             print('Your can also add boilerplates for your project:')
             print('$ pyx add {framework}')
+
+            for framework in frameworks:
+                _add_framework(project_name, category_id, framework)
 
     except FileExistsError:
         print('Destination directory is already exist. Consider another name of the project.')
@@ -177,36 +274,8 @@ def add(args, pyx_project, **kwargs):
     """
     Add model to the project
     """
-    import os
-    import pyx
-
-    pyx_boilerplate_path = os.path.join(
-        os.path.dirname(pyx.__file__), 'boilerplates',
-        pyx_project['category'],
-        args.framework
-    )
-
-    if not os.path.exists(pyx_boilerplate_path):
-        pyx_boilerplate_path = os.path.join(
-            os.path.dirname(pyx.__file__), 'boilerplates', 'general',
-            args.framework
-        )
-
-    if os.path.exists(pyx_boilerplate_path):
-        print('Adding model ...')
-        try:
-            from shutil import copytree
-            copytree(pyx_boilerplate_path, os.path.join('models', args.framework))
-            print('...')
-            print('Your boilerplate is ready to use. Please, run:')
-            print('$ pyx run ' + args.framework)
-            print('or :')
-            print('$ cd models/{}'.format(args.framework))
-            print('$ python PYX.py')
-        except FileExistsError:
-            print('Destination directory is already exist. Consider another name of the project.')
-    else:
-        print('Template for category {0} not found'.format(args.category))
+    print(args, kwargs)
+    _add_framework('./', pyx_project['category_id'], args.framework)
 
 
 @ensure_pyx_project
@@ -262,13 +331,42 @@ def test(*args, pyx_project, **kwargs):
             print('....')
             print('PASSED')
             sys.path.pop()
-        except:
+        except Exception as e:
+            print(e.with_traceback())
             print('....')
             print('An error occured.')
             return False
 
-        return True
+    return True
 
+
+@ensure_pyx_project
+def publish(args, pyx_project, **kwargs):
+    questions = [
+        inquirer.Text('name',
+                      message="Please, specify model name", default=pyx_project['name']),
+        inquirer.Text('paper',
+                      message="Please, specify paper url if you have one", default=pyx_project['paper']),
+        inquirer.Text('dataset',
+                      message="Please, specify paper dataset you used", default=pyx_project['dataset']),
+        inquirer.Text('license',
+                      message="Please, specify license", default=pyx_project['license']),
+        inquirer.Editor('description_short', message="Please, specify short description of your model",
+                        default=pyx_project['description_short']),
+        inquirer.Editor('description_full', message="Please, specify short detailed of your model",
+                        default=pyx_project['description_full']),
+    ]
+    answers = inquirer.prompt(questions)
+    for k in answers:
+        pyx_project[k] = answers[k]
+
+    import requests
+    from urllib.parse import urljoin
+    headers = {'user-token': __PYX_CONFIG__["user_token"]}
+    r = requests.post(urljoin(__PYX_CONFIG__["api_url"], 'models'),
+                      headers=headers,
+                      json=json.dumps(pyx_project))
+    print(r.status_code)
 
 @ensure_pyx_project
 @ensure_have_permissions
@@ -346,7 +444,7 @@ def predict(args, extra_fields):
     headers = {'user-token': __PYX_CONFIG__["user_token"]}
     r = requests.post(urljoin(__PYX_CONFIG__["api_url"], 'models/' + model_id + '/predict/' + framework),
                       headers=headers, json=data)
-    print(r.status_code, r.content)
+
     if r.status_code == 200:
         print('Succesfully predicted ...', r.json())
     else:
@@ -370,7 +468,7 @@ def quotas(args, extra_fields):
 
 def main():
     _load_config()
-    print(__PYX_CONFIG__)
+    # print(__PYX_CONFIG__)
     parser = argparse.ArgumentParser(prog='pyx')
 
     subparsers = parser.add_subparsers(dest='mode', help='sub-command help')
@@ -380,10 +478,7 @@ def main():
 
     parser_list_templates = subparsers.add_parser('list-templates', help='List available templates')
 
-    parser_create = subparsers.add_parser('create', help='Create new PYX project')
-    parser_create.add_argument('project_name', type=str, help='a name of new project (e.g. resnet-test)')
-    parser_create.add_argument('category', type=str, help='a category of a new project (e.g. cv/classification)')
-
+    parser_create = subparsers.add_parser('create', help='Create new PYX project (with wizard)')
     parser_config = subparsers.add_parser('config', help='Configure PYX project')
 
     parser_create = subparsers.add_parser('add', help='Add model to the project')
@@ -393,6 +488,7 @@ def main():
     parser_download.add_argument('model_id', type=str, help='model id from pyx.ai')
     parser_download.add_argument('project_name', type=str, help='destination project name')
 
+    parser_publish = subparsers.add_parser('publish', help='Publish project to pyx.ai')
     parser_upload = subparsers.add_parser('upload', help='Upload current workspace to pyx.ai')
 
     parser_predict = subparsers.add_parser('predict', help='Push current workspace to pyx.ai')
@@ -422,6 +518,7 @@ def main():
         'list-templates': list_templates,
         'add': add,
         'test': test,
+        'publish': publish,
         'upload': upload,
         'download': download,
         'predict': predict,
